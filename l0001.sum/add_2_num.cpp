017283ecc6a7eb20a02739852460c95336688dce
sjtu_huye <283989318@qq.com>
2019-10-31 00:10:48 +0800

    新增l0001.add

---
+++ l0001.sum/add_2_num.cpp
@@ -0,0 +1,21 @@
+/***********************************************************************
+ * @file add_2_num.cpp
+ * @brief  leetcode file
+ * @history
+ * Date       Version Author    description
+ * ========== ======= ========= =======================================
+ * 2019-10-30 V1.0    HuYe.SJTU   Create
+ *
+ * @Copyright (C)  2019  .China . all right reserved
+***********************************************************************/
+
+#include "stdio.h"
+
+int main()
+{
+    int n;
+    while(scanf("%d", &n) != EOF) {
+        printf("%d\n", n);
+    }
+    return 0;
+}
